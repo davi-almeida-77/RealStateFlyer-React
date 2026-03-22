a94e7ddbe4b1d0bd8171865560dc70060358851d
davi-almeida-77 <DaviAlmeidaDEV@hotmail.com>
2026-03-22 19:53:53 -0300

    [UPDATE] Config For Dark White Theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,21 @@
 import { Routes, Route } from 'react-router-dom'
-import PropertyPage from '@/pages/PropertyPage'
-import CustomCursor from '@/components/ui/CustomCursor'
+import { ThemeProvider } from '@/context/ThemeContext'
+import HomePage       from '@/pages/HomePage'
+import PropertyPage   from '@/pages/PropertyPage'
+import ContactPage    from '@/pages/ContactPage'
+import CustomCursor   from '@/components/ui/CustomCursor'
 import ScrollProgress from '@/components/ui/ScrollProgress'
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider>
       <CustomCursor />
       <ScrollProgress />
       <Routes>
-        <Route path="/"             element={<PropertyPage />} />
+        <Route path="/"             element={<HomePage />} />
         <Route path="/property/:id" element={<PropertyPage />} />
+        <Route path="/contact"      element={<ContactPage />} />
       </Routes>
-    </>
+    </ThemeProvider>
   )
 }
